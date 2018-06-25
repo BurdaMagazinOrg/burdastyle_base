73cb3e8e9c701e7a610cc76df7e1ff6c13ee2ae2
Steffen Schlaer <steffen.schlaer@it-cru.de>
2018-06-25 14:55:27 +0200

    INREL-4043: Add missing code for handling ecommerce slider field_css_styles

---
+++ modules/burdastyle_thunder_admin/js/field-css-styles.js
@@ -0,0 +1,7 @@
+(function ($) {
+    $('.field--name-field-css-styles textarea').click(function(){
+        if (!$('.field--name-field-css-styles textarea').val()) {
+            $('.field--name-field-css-styles textarea').val('.field--name-field-css-styles textarea').attr('placeholder');
+        }
+    });
+}(jQuery));
