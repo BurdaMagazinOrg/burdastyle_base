ba6f106c227332850671796b45670325b91096fb
Dan Schalow <daniel.schalow@burda.com>
2018-08-24 11:53:55 +0200

    INREL-4521: considered opened paragraphs and CKEDITOR changes too

--- modules/burdastyle_thunder_admin/js/edit-plus.js
+++ modules/burdastyle_thunder_admin/js/edit-plus.js
@@ -1,18 +1,39 @@
-Drupal.behaviors.editPlus = {
+/* eslint-disable no-alert */
+
+const handleEditedForms = {
   formHasChanged: false,
-  attach(context) {
-    jQuery('input, select, textarea').on(
-        'change',
-        () => {
-          if (false === this.formHasChanged) {
-            jQuery('.tabs a[href$="/edit"], .tabs a[href$="/edit_plus"]').on('click', (e) => {
-              const answer = confirm('You have unsaved changes. Do you really want to leave this site?');
-              if (false === answer)
-                e.preventDefault();
-            });
-          }
-          this.formHasChanged = true;
+  form: document.querySelector('#node-article-edit-form, #node-article-edit-plus-form'),
+  handleClick(e) {
+    const querySelector = '.tabs a[href$="/edit"], .tabs a[href$="/edit_plus"]';
+    const message = 'You have unsaved changes. Do you really want to leave this site?';
+    const clickedNavigation = e.target.matches(querySelector);
+
+    if (clickedNavigation && this.formHasChanged) {
+      const answer = window.confirm(message);
+      if (!answer) e.preventDefault();
+    }
+  },
+  handleFormChange() {
+    this.formHasChanged = true;
+  },
+  handleMutations(mutationList) {
+    const isChildList = !!mutationList.find(mutation => mutation.type === 'childList');
+    if (isChildList) {
+      Object.keys(CKEDITOR.instances).forEach((editor) => {
+        const instance = CKEDITOR.instances[editor];
+        if (!instance.listenerAdded) {
+          instance.on('change', e => this.handleFormChange(e));
+          instance.listenerAdded = true;
         }
-    );
+      });
+    }
+  },
+  init() {
+    const observer = new MutationObserver(this.handleMutations.bind(this));
+    observer.observe(this.form, { attributes: false, childList: true, subtree: true });
+    this.form.addEventListener('change', this.handleFormChange.bind(this));
+    document.body.addEventListener('click', this.handleClick.bind(this));
   },
 };
+
+document.addEventListener('DOMContentLoaded', handleEditedForms.init.bind(handleEditedForms));
